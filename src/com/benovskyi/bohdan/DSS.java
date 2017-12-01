c1e22d145190d9c2294ad8ee284fbd1fbcefec44
BohdanBenovskyi <benovskiy1994@gmail.com>
2017-12-01 19:36:02 +0200

    calculate open key Y for signature of sender

--- src/com/benovskyi/bohdan/DSS.java
+++ src/com/benovskyi/bohdan/DSS.java
@@ -7,9 +7,22 @@ public class DSS {
 	public static BigInteger p;
 	public static BigInteger g;
 	public static BigInteger q;
+	public static BigInteger y;
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter your message: ");
+		String msg = in.nextLine();
+		
 		gen_pgq();
+		
+		Random rnd = new Random();
+		int x = rnd.nextInt(q.intValue());
+		System.out.println("x = " + x);
+		calc_Y(x);
+		
+		BigInteger m = calcIntHash(msg);
+		
 	}
 	
 	public static void gen_pgq() {
@@ -38,6 +51,23 @@ public class DSS {
 		System.out.println("p = " + p);
 		System.out.println("g = " + g);
 		System.out.println("q = " + q);
+	}
+	
+	public static void calc_Y(int x) {
+		BigInteger tmp_1 = g.pow(x);
+		y = tmp_1.mod(p);
+		System.out.println("y = " + y);
+	}
+	
+	public static BigInteger calcIntHash(String msg) {
+		BigInteger hash;
+		int h = 0;
+		for(int i = 0; i < msg.length(); i++) {
+			h += msg.codePointAt(i);
+		}
+		hash = new BigInteger(String.valueOf(h));
+		System.out.println("hash = " + hash);
+		return hash;
 	}
 
 }
