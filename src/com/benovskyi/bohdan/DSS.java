c70720c3c15d185a52b6439a4860b64431cbd96f
BohdanBenovskyi <benovskiy1994@gmail.com>
2017-12-01 18:43:43 +0200

    generate G big number

--- src/com/benovskyi/bohdan/DSS.java
+++ src/com/benovskyi/bohdan/DSS.java
@@ -4,40 +4,39 @@ import java.util.*;
 import java.math.*;
 
 public class DSS {
-	public static BigInteger q;
 	public static BigInteger p;
+	public static BigInteger g;
+	public static BigInteger q;
 
 	public static void main(String[] args) {
-		gen_pq();
-		gen_pq();
+		gen_pgq();
 	}
 	
-	public static void gen_pq() {
+	public static void gen_pgq() {
 		Random rnd = new Random();
-		int bitLength = 15;
+		int bitLength = 20;
 		p = BigInteger.probablePrime(bitLength, rnd);
+		g = BigInteger.probablePrime(bitLength, rnd);
 		
 		
 		q = new BigInteger("2");
 		
-		System.out.println("We found p: " + p);
 		while(q.compareTo(p.subtract(BigInteger.ONE)) == -1) {
 			q = q.add(BigInteger.ONE);
 			
 			if(q.isProbablePrime(1)) {
 				BigInteger tmp_mod = (p.subtract(BigInteger.ONE)).mod(q);
 				if(tmp_mod.equals(BigInteger.ZERO)) {
-					System.out.println("We found q: " + q);
 					break;
 				}
 			}
 			else {
-				System.out.println("Not found: " + q);
 				continue; 
 			}
 		}
 		
 		System.out.println("p = " + p);
+		System.out.println("g = " + g);
 		System.out.println("q = " + q);
 	}
 
